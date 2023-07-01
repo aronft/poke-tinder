3da20a86612aa825edf57ab4ef6091b09053c8b2
blazte <blazte.aster@gmail.com>
2023-07-01 09:06:48 -0500

    install prettier and eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,21 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-}
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".tsx", ".ts"] }],
+  },
+};
